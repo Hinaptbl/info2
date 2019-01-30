f967d0354e914e7610bade45785e1a95fcbd9397
Hina Sakugawa <44792873+Hinaptbl@users.noreply.github.com>
2019-01-30 22:27:43 +0900

    Add files via upload

--- final.js
+++ final.js
@@ -2,8 +2,6 @@
 function inuneko(){
    var point=0;
 
-
-
 if(document.question.a[0].checked){point+=1}
 if(document.question.a[1].checked){point+=0}
 
@@ -16,7 +14,9 @@ if(document.question.c[1].checked){point+=100}
 if(point>200){      ///マップへ
  location.href="finalp2.html";
 }
-if(point<=200){　　　　　　　　///署名へ
+else if(point<=200 && 0<point){　　　　　　　　///署名へ
  location.href="finalp3.html";
+}else{
+  alert("項目が選択されていません。");
 }
 }
